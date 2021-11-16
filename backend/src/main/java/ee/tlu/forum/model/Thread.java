3d951539a9e15b17ce5875124ba0be846e363b6f
Uptaker <markus.tammeoja@gmail.com>
2021-11-16 23:01:37 +0200

    Add base thread model. Closes #4

---
+++ backend/src/main/java/ee/tlu/forum/model/Thread.java
@@ -0,0 +1,28 @@
+package ee.tlu.forum.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Thread extends BaseEntity {
+
+    @Id
+    private Long id;
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User author;
+
+    @OneToMany(mappedBy = "thread", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<Post> post;
+}
