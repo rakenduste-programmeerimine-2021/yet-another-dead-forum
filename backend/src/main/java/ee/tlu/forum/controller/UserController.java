0e683e5e667b5d3bf82dca51a3937eb46932ac9d
Uptaker <markus.tammeoja@gmail.com>
2021-11-17 14:57:30 +0200

    Get user by ID. Closes #10

--- backend/src/main/java/ee/tlu/forum/controller/UserController.java
+++ backend/src/main/java/ee/tlu/forum/controller/UserController.java
@@ -3,6 +3,7 @@ package ee.tlu.forum.controller;
 import ee.tlu.forum.model.User;
 import ee.tlu.forum.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ public class UserController {
     @GetMapping("/")
     public List<User> getUsers() {
         return userService.getUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
     }
 }
--- backend/src/main/java/ee/tlu/forum/service/UserService.java
+++ backend/src/main/java/ee/tlu/forum/service/UserService.java
@@ -19,4 +19,8 @@ public class UserService {
     public List<User> getUsers() {
         return userRepository.findAll();
     }
+
+    public User getUserById(Long id) {
+        return userRepository.getById(id);
+    }
 }
