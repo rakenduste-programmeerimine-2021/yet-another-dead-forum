1efdabf06acacb0fed2223c2019d4d368dc16bd9
Uptaker <markus.tammeoja@gmail.com>
2021-11-17 14:52:30 +0200

    Get all users. Closes #7

---
+++ backend/src/main/java/ee/tlu/forum/controller/UserController.java
@@ -0,0 +1,25 @@
+package ee.tlu.forum.controller;
+
+import ee.tlu.forum.model.User;
+import ee.tlu.forum.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+}
---
+++ backend/src/main/java/ee/tlu/forum/service/UserService.java
@@ -0,0 +1,22 @@
+package ee.tlu.forum.service;
+
+import ee.tlu.forum.model.User;
+import ee.tlu.forum.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+}
