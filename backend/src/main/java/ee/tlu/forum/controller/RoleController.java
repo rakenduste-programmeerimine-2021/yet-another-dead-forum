d976f48757ba737bc3e913112e39f078478aa6a7
Uptaker <markus.tammeoja@gmail.com>
2021-11-17 20:10:37 +0200

    Add Role API endpoints

---
+++ backend/src/main/java/ee/tlu/forum/controller/RoleController.java
@@ -0,0 +1,47 @@
+package ee.tlu.forum.controller;
+
+import ee.tlu.forum.model.Role;
+import ee.tlu.forum.service.UserService;
+import lombok.Data;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api")
+public class RoleController {
+
+    UserService userService;
+
+    public RoleController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/roles")
+    public ResponseEntity<List<Role>> getRoles() {
+        return ResponseEntity.ok().body(userService.getRoles());
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        // ServletUriComponentsBuilder.fromCurrentContextPath() - returns current link up to the port (8080)
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?> saveRole(@RequestBody AddRoleToUserForm form) {
+        userService.addRoleToUser(form.getUserName(), form.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+}
+
+@Data
+class AddRoleToUserForm {
+    private String userName;
+    private String roleName;
+}
