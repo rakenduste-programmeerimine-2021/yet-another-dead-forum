e82f85424ac1b6b94e333a3afdad7e28e56b2269
Uptaker <markus.tammeoja@gmail.com>
2021-11-17 20:06:14 +0200

    Add user creation. Closes #9

--- backend/src/main/java/ee/tlu/forum/controller/UserController.java
+++ backend/src/main/java/ee/tlu/forum/controller/UserController.java
@@ -4,12 +4,14 @@ import ee.tlu.forum.model.User;
 import ee.tlu.forum.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
-@RequestMapping("/api/user")
+@RequestMapping("/api")
 public class UserController {
 
     UserService userService;
@@ -18,12 +20,19 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/")
+    @GetMapping("/users")
     public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok().body(userService.getUsers());
     }
 
-    @GetMapping("/{id}")
+    @PostMapping("/user/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        // ServletUriComponentsBuilder.fromCurrentContextPath() - returns current link up to the port (8080)
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @GetMapping("/user/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
         return ResponseEntity.ok().body(userService.getUserById(id));
     }
