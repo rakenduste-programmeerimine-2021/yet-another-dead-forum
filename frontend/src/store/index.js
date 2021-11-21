51eb9c45f2ca75c16c056aa24904fee1d68eee50
Neniariel <42422684+Neniariel@users.noreply.github.com>
2021-11-21 18:11:39 +0200

    Set up Store

---
+++ frontend/src/store/index.js
@@ -0,0 +1,26 @@
+import { createContext, useReducer } from 'react';
+import combineReducers from 'react-combine-reducers';
+import { authReducer } from './reducer';
+
+const initialAuth = {
+  token: null,
+  user: null,
+};
+
+const [combinedReducer, initialState] = combineReducers({
+  auth: [authReducer, initialAuth]
+});
+
+export const Context = createContext(initialState);
+
+const Store = ({ children }) => {
+  const [state, dispatch] = useReducer(combinedReducer, initialState);
+
+  return (
+    <Context.Provider value={[ state, dispatch ]}>
+      { children }
+    </Context.Provider>
+  );
+};
+
+export default Store;
