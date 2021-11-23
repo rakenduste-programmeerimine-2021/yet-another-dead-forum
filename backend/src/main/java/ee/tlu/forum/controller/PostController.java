4651c23e6c518b2ecba6a687950742ad992be65a
Uptaker <dr1nky@tlu.ee>
2021-11-23 21:00:48 +0200

    Closes #8. Closes #11. Closes #13. Closes #15. Closes #17. Closes #20

---
+++ backend/src/main/java/ee/tlu/forum/controller/PostController.java
@@ -0,0 +1,60 @@
+package ee.tlu.forum.controller;
+
+import ee.tlu.forum.model.Post;
+import ee.tlu.forum.model.input.AddNewPostInput;
+import ee.tlu.forum.service.PostService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api")
+public class PostController {
+    
+    PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/posts")
+    public ResponseEntity<List<Post>> getPosts() {
+        return ResponseEntity.ok().body(postService.getAllPosts());
+    }
+
+    @GetMapping("/post/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(postService.getPostById(id));
+    }
+
+    @PostMapping("/post/add")
+    public ResponseEntity<Post> createPost(@RequestBody AddNewPostInput form) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("api/post/add").toUriString());
+        return ResponseEntity.created(uri).body(postService.createPost(form));
+    }
+
+    @PatchMapping("/post/edit")
+    public ResponseEntity<Post> editPost(@RequestBody Post post) {
+        return ResponseEntity.ok().body(postService.editPost(post));
+    }
+
+    @DeleteMapping("/post/delete/{id}")
+    public ResponseEntity<?> deletePost(@PathVariable Long id) {
+        postService.deletePostById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/posts/userid/{id}")
+    public ResponseEntity<List<Post>> getPostsByUserId(@PathVariable Long id) {
+        return ResponseEntity.ok().body(postService.getAllPostsByUserId(id));
+    }
+
+    @GetMapping("/posts/username/{name}")
+    public ResponseEntity<List<Post>> getPostsByUsername(@PathVariable String name) {
+        return ResponseEntity.ok().body(postService.getAllPostsByUsername(name));
+    }
+}
