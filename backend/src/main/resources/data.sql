612f10a75b84b4ce6544b13bdbc744862ae5acfe
Uptaker <markus.tammeoja@gmail.com>
2021-12-01 18:39:52 +0200

    Removed possibility of NULL in user model

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -4,9 +4,9 @@ INSERT INTO public.role(name) VALUES ('ROLE_MODERATOR') ON CONFLICT DO NOTHING;
 INSERT INTO public.role(name) VALUES ('ROLE_PREMIUM') ON CONFLICT DO NOTHING;
 
 -- username 'Testing', password is 'test'
-INSERT INTO public.users (created_at, updated_at, about, email, password, signature, username) VALUES ('2021-11-20 21:22:36.115454', '2021-11-20 21:22:36.116453', null, 'test@test.com', '$2a$10$xnUvv01NPMsvYQEYINiACOZTADPCoD9N6jjd7LSS4tYoYPZNdmXEG', null, 'Testing') ON CONFLICT DO NOTHING;
-INSERT INTO public.users (created_at, updated_at, about, email, password, signature, username) VALUES ('2021-11-20 21:22:36.115454', '2021-11-20 21:22:36.116453', null, 'foo@test.com', '$2a$10$xnUvv01NPMsvYQEYINiACOZTADPCoD9N6jjd7LSS4tYoYPZNdmXEG', null, 'Foo') ON CONFLICT DO NOTHING;
-INSERT INTO public.users (created_at, updated_at, about, email, password, signature, username) VALUES ('2021-11-20 21:22:36.115454', '2021-11-20 21:22:36.116453', null, 'bar@test.com', '$2a$10$xnUvv01NPMsvYQEYINiACOZTADPCoD9N6jjd7LSS4tYoYPZNdmXEG', null, 'Bar') ON CONFLICT DO NOTHING;
+INSERT INTO public.users (created_at, updated_at, about, email, password, signature, username) VALUES ('2021-11-20 21:22:36.115454', '2021-11-20 21:22:36.116453', '', 'test@test.com', '$2a$10$xnUvv01NPMsvYQEYINiACOZTADPCoD9N6jjd7LSS4tYoYPZNdmXEG', '', 'Testing') ON CONFLICT DO NOTHING;
+INSERT INTO public.users (created_at, updated_at, about, email, password, signature, username) VALUES ('2021-11-20 21:22:36.115454', '2021-11-20 21:22:36.116453', '', 'foo@test.com', '$2a$10$xnUvv01NPMsvYQEYINiACOZTADPCoD9N6jjd7LSS4tYoYPZNdmXEG', '', 'Foo') ON CONFLICT DO NOTHING;
+INSERT INTO public.users (created_at, updated_at, about, email, password, signature, username) VALUES ('2021-11-20 21:22:36.115454', '2021-11-20 21:22:36.116453', '', 'bar@test.com', '$2a$10$xnUvv01NPMsvYQEYINiACOZTADPCoD9N6jjd7LSS4tYoYPZNdmXEG', '', 'Bar') ON CONFLICT DO NOTHING;
 
 INSERT INTO public.users_roles (user_id, roles_id)
 SELECT
