01c038c9f43942f285df616fd7ef5674286d14be
Uptaker <markus.tammeoja@gmail.com>
2021-12-13 15:03:18 +0200

    Latest threads are now first

--- backend/src/main/java/ee/tlu/forum/repository/ThreadRepository.java
+++ backend/src/main/java/ee/tlu/forum/repository/ThreadRepository.java
@@ -3,5 +3,8 @@ package ee.tlu.forum.repository;
 import ee.tlu.forum.model.Thread;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ThreadRepository extends JpaRepository<Thread, Long> {
+    public List<Thread> findAllByOrderByIdDesc();
 }
