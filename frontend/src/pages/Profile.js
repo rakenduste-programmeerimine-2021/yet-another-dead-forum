1c6fdf2f8bc92d22f7664f3b52194dec45f71c84
Uptaker <markus.tammeoja@gmail.com>
2021-12-13 18:51:58 +0200

    Posts page loads user data

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,9 +1,38 @@
-import { Typography } from 'antd';
+import {Spin, Typography} from 'antd';
+import {useContext, useEffect, useState} from "react";
+import {Context} from "../store";
+import {useParams} from "react-router-dom";
 
 const Profile = () => {
-  const { Title } = Typography;
+
+    const [profile, setProfile] = useState()
+    const [state, dispatch] = useContext(Context)
+    const { Title } = Typography
+    const params = useParams()
+
+    useEffect(async () => {
+        setProfile(await fetch(`${process.env.REACT_APP_SITE_URL}:8080/api/user/${params.username}/profile`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + state.auth.token
+            }
+        }).then(r => r.json()))
+    }, [])
+
   return(
-    <Title style={{textAlign: 'center'}}>Profile</Title>
+    <>
+        {
+            profile
+                ?
+            <>
+            <Title style={{textAlign: 'center'}}>{profile.displayName}'s profile</Title>
+            <div>Test</div>
+            </>
+                :
+            <Spin />
+        }
+    </>
   )
 };
 
