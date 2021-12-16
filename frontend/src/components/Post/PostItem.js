8786ec734beae31d563f96231a474ec349991f4a
Uptaker <markus.tammeoja@gmail.com>
2021-12-16 18:02:03 +0200

    Fixed broken post layout

--- frontend/src/components/Post/PostItem.js
+++ frontend/src/components/Post/PostItem.js
@@ -41,39 +41,41 @@ const PostItem = ({post}) => {
   }
 
   return (
-    <List.Item style={{display: 'flex', gap: '15px'}} className={'content-list'}>
-      <div style={{display: 'flex', flexDirection: 'column', flex: '0 0 10em'}}>
-        <span><Link to={"/user/" + post.author.username}>{post.author.displayName}</Link></span>
-        <span>Posts: {post.author.threadAmount + post.author.postAmount}</span>
-        <span>Join date: {format(new Date(post.author.createdAt), 'dd. MMM yyyy')}</span>
-        {post.author.roles.map((role, i) => (
-          ((role.id === 1 && post.author.roles.length === 1) || (role.id !== 1 && post.author.roles.length > 1)) &&
-          <div key={i} style={{backgroundColor: role.bodyCss, textAlign: 'center', marginBottom: '5px'}}>
-            <Text style={{color: role.textCss, marginLeft: '5px'}}>{role.displayName}</Text>
-          </div>
-        ))}
-      </div>
-      <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-        <Text style={{marginBottom: '20px'}}>{post.text}</Text>
-        {(post.author.signature && post.author.signature.length) &&
-        <div><Divider dashed/><p>{post.author.signature}</p><Divider dashed/></div>}
-        <Space size={"large"} style={{display: 'flex', width: '100%'}}>
-          {post.createdAt && <Text style={{fontSize: '11px'}}
-                                   type='secondary'><ClockCircleOutlined/> {format(new Date(post.createdAt), 'dd. MMM yyyy')}
-          </Text>}
-          {
-            Math.round(created.getTime() / 1000) < Math.round(updated.getTime() / 1000) &&
-            <Text type="secondary" style={{fontSize: '11px', fontStyle: 'italic'}}>Edited</Text>
-          }
-          {state.auth.token &&
-          (state.auth.user.roles.includes('ROLE_ADMIN') || state.auth.user.roles.includes('ROLE_MODERATOR')) &&
-          <span className="delete" onClick={() => postDelete(post)}>DELETE</span>
-          }
-          {state.auth.token &&
-          (state.auth.user.roles.includes('ROLE_ADMIN') || state.auth.user.roles.includes('ROLE_MODERATOR') || parseInt(state.auth.user.id, 10) === post.author.id) &&
-          <Link className="edit" to={"/post/edit/" + post.id}> EDIT</Link>
-          }
-        </Space>
+    <List.Item>
+      <div className={'content-list'} style={{display: 'flex', gap: '15px'}}>
+        <div style={{display: 'flex', flexDirection: 'column', flex: '0 0 10em'}}>
+          <span><Link to={"/user/" + post.author.username}>{post.author.displayName}</Link></span>
+          <span>Posts: {post.author.threadAmount + post.author.postAmount}</span>
+          <span>Join date: {format(new Date(post.author.createdAt), 'dd. MMM yyyy')}</span>
+          {post.author.roles.map((role, i) => (
+            ((role.id === 1 && post.author.roles.length === 1) || (role.id !== 1 && post.author.roles.length > 1)) &&
+            <div key={i} style={{backgroundColor: role.bodyCss, textAlign: 'center', marginBottom: '5px'}}>
+              <Text style={{color: role.textCss, marginLeft: '5px'}}>{role.displayName}</Text>
+            </div>
+          ))}
+        </div>
+        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+          <Text style={{marginBottom: '20px'}}>{post.text}</Text>
+          {(post.author.signature && post.author.signature.length) &&
+          <div><Divider dashed/><p>{post.author.signature}</p><Divider dashed/></div>}
+          <Space size={"large"} style={{display: 'flex', width: '100%'}}>
+            {post.createdAt && <Text style={{fontSize: '11px'}}
+                                     type='secondary'><ClockCircleOutlined/> {format(new Date(post.createdAt), 'dd. MMM yyyy')}
+            </Text>}
+            {
+              Math.round(created.getTime() / 1000) < Math.round(updated.getTime() / 1000) &&
+              <Text type="secondary" style={{fontSize: '11px', fontStyle: 'italic'}}>Edited</Text>
+            }
+            {state.auth.token &&
+            (state.auth.user.roles.includes('ROLE_ADMIN') || state.auth.user.roles.includes('ROLE_MODERATOR')) &&
+            <span className="delete" onClick={() => postDelete(post)}>DELETE</span>
+            }
+            {state.auth.token &&
+            (state.auth.user.roles.includes('ROLE_ADMIN') || state.auth.user.roles.includes('ROLE_MODERATOR') || parseInt(state.auth.user.id, 10) === post.author.id) &&
+            <Link className="edit" to={"/post/edit/" + post.id}> EDIT</Link>
+            }
+          </Space>
+        </div>
       </div>
     </List.Item>
   )
