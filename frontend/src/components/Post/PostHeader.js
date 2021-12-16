7a6f515b32f34cac5f910b1cb1a73677f1d4daeb
Uptaker <markus.tammeoja@gmail.com>
2021-12-16 13:43:03 +0200

    Format code

--- frontend/src/components/Post/PostHeader.js
+++ frontend/src/components/Post/PostHeader.js
@@ -1,15 +1,15 @@
-import { useContext, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { format } from 'date-fns';
+import {useContext, useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {format} from 'date-fns';
 import {Divider, Space, Typography} from 'antd';
-import { Context } from '../../store';
-import { deleteThread } from '../../store/actions';
+import {Context} from '../../store';
+import {deleteThread} from '../../store/actions';
 import {ClockCircleOutlined} from "@ant-design/icons";
 
-const PostHeader = ({ data }) => {
+const PostHeader = ({data}) => {
   const [error, setError] = useState('');
   const [state, dispatch] = useContext(Context);
-  const { Text, Title } = Typography;
+  const {Text, Title} = Typography;
   const navigate = useNavigate();
 
   const created = new Date(data.createdAt)
@@ -23,53 +23,56 @@ const PostHeader = ({ data }) => {
           'Content-Type': 'application/json',
           'Authorization': 'Bearer ' + state.auth.token
         },
-        body: JSON.stringify({ id: data.id }),
+        body: JSON.stringify({id: data.id}),
       })
 
-      if(res.ok) {
+      if (res.ok) {
         dispatch(deleteThread(data))
-        navigate('/', { replace: true });
+        navigate('/', {replace: true});
       } else {
         let errors = ''
-          if (res.json().error) {
-            errors = res.json().error
-          } else {
-            for (let i = 0; i < res.json().msg.length; i++) {
-              errors += res.json().msg[i].param[0].toUpperCase() + res.json().msg[i].param.slice(1) + ' ' + res.json().msg[i].msg + '\n'
-            }
+        if (res.json().error) {
+          errors = res.json().error
+        } else {
+          for (let i = 0; i < res.json().msg.length; i++) {
+            errors += res.json().msg[i].param[0].toUpperCase() + res.json().msg[i].param.slice(1) + ' ' + res.json().msg[i].msg + '\n'
           }
-          setError(errors)
+        }
+        setError(errors)
       }
     }
   }
 
-  return(
+  return (
     <>
-      { error && <Text style={{whiteSpace: 'pre-wrap'}} type="danger">{ error }</Text> }
+      {error && <Text style={{whiteSpace: 'pre-wrap'}} type="danger">{error}</Text>}
       <div className={'content-list'} style={{display: 'flex', gap: '15px'}}>
         <div style={{display: 'inline-flex', flexDirection: 'column', flex: '0 0 10em'}}>
           <span><Link to={"/user/" + data.author.username}>{data.author.displayName}</Link></span>
           <span>Posts: {data.author.threadAmount + data.author.postAmount}</span>
           <span>Join date: {format(new Date(data.author.createdAt), 'dd. MMM yyyy')}</span>
           {data.author.roles.map((role, i) => (
-            ((role.id === 1 && data.author.roles.length === 1 ) || (role.id !== 1 && data.author.roles.length > 1)) &&
-            <div key={i} style={{backgroundColor: role.bodyCss, textAlign: 'center', marginBottom:'5px'}}>
+            ((role.id === 1 && data.author.roles.length === 1) || (role.id !== 1 && data.author.roles.length > 1)) &&
+            <div key={i} style={{backgroundColor: role.bodyCss, textAlign: 'center', marginBottom: '5px'}}>
               <Text style={{color: role.textCss, marginLeft: '5px'}}>{role.displayName}</Text>
             </div>
           ))}
         </div>
-        <div style={{display: 'flex', flexDirection: 'column', justifyContent:'space-between'}}>
+        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
           <Title level={5}>{data.title}</Title>
           <Text>{data.text}</Text>
-          {(data.author.signature && data.author.signature.length) && <div><Divider dashed/><p>{data.author.signature}</p><Divider dashed/></div>}
+          {(data.author.signature && data.author.signature.length) &&
+          <div><Divider dashed/><p>{data.author.signature}</p><Divider dashed/></div>}
           <Space size="large">
-            <Space id="test"  size={"large"}>
-              {data.createdAt && <Text type='secondary' style={{ fontSize: '11px'}} ><ClockCircleOutlined /> {format(new Date(data.createdAt), 'dd. MMM yyyy')}</Text>}
+            <Space id="test" size={"large"}>
+              {data.createdAt && <Text type='secondary'
+                                       style={{fontSize: '11px'}}><ClockCircleOutlined/> {format(new Date(data.createdAt), 'dd. MMM yyyy')}
+              </Text>}
             </Space>
-          {
-            Math.round(created.getTime() / 1000) < Math.round(updated.getTime() / 1000) &&
-            <Text type="secondary" style={{ fontSize: '11px', fontStyle: 'italic' }}>Edited</Text>
-          }
+            {
+              Math.round(created.getTime() / 1000) < Math.round(updated.getTime() / 1000) &&
+              <Text type="secondary" style={{fontSize: '11px', fontStyle: 'italic'}}>Edited</Text>
+            }
             {state.auth.token &&
             <>
               {
--- frontend/src/components/Post/PostItem.js
+++ frontend/src/components/Post/PostItem.js
@@ -1,14 +1,14 @@
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { format } from 'date-fns';
+import {useContext} from 'react';
+import {Link} from 'react-router-dom';
+import {format} from 'date-fns';
 import {Divider, List, Space, Typography} from 'antd';
-import { Context } from '../../store';
-import { deletePost } from '../../store/actions';
+import {Context} from '../../store';
+import {deletePost} from '../../store/actions';
 import {ClockCircleOutlined} from "@ant-design/icons";
 
-const PostItem = ({ post }) => {
+const PostItem = ({post}) => {
   const [state, dispatch] = useContext(Context);
-  const { Text } = Typography;
+  const {Text} = Typography;
 
   const created = new Date(post.createdAt)
   const updated = new Date(post.updatedAt)
@@ -21,57 +21,60 @@ const PostItem = ({ post }) => {
           'Content-Type': 'application/json',
           'Authorization': 'Bearer ' + state.auth.token
         },
-        body: JSON.stringify({ id: data.id }),
+        body: JSON.stringify({id: data.id}),
       })
 
-      if(res.ok) {
+      if (res.ok) {
         dispatch(deletePost(data))
       } else {
         let errors = ''
-          if (res.json().error) {
-            errors = res.json().error
-          } else {
-            for (let i = 0; i < res.json().msg.length; i++) {
-              errors += res.json().msg[i].param[0].toUpperCase() + res.json().msg[i].param.slice(1) + ' ' + res.json().msg[i].msg + '\n'
-            }
+        if (res.json().error) {
+          errors = res.json().error
+        } else {
+          for (let i = 0; i < res.json().msg.length; i++) {
+            errors += res.json().msg[i].param[0].toUpperCase() + res.json().msg[i].param.slice(1) + ' ' + res.json().msg[i].msg + '\n'
           }
-          console.log(errors)
+        }
+        console.log(errors)
       }
     }
   }
 
-  return(
+  return (
     <List.Item style={{display: 'flex', gap: '15px'}} className={'content-list'}>
-        <div style={{display: 'flex', flexDirection: 'column', flex: '0 0 10em'}}>
+      <div style={{display: 'flex', flexDirection: 'column', flex: '0 0 10em'}}>
         <span><Link to={"/user/" + post.author.username}>{post.author.displayName}</Link></span>
-          <span>Posts: {post.author.threadAmount + post.author.postAmount}</span>
-          <span>Join date: {format(new Date(post.author.createdAt), 'dd. MMM yyyy')}</span>
-          {post.author.roles.map((role, i) => (
-            ((role.id === 1 && post.author.roles.length === 1 ) || (role.id !== 1 && post.author.roles.length > 1)) &&
-            <div key={i} style={{backgroundColor: role.bodyCss, textAlign: 'center', marginBottom:'5px'}}>
-              <Text style={{color: role.textCss, marginLeft: '5px'}}>{role.displayName}</Text>
-            </div>
-          ))}
-        </div>
-        <div style={{display: 'flex', flexDirection: 'column', justifyContent:'space-between'}}>
-          <Text style={{marginBottom: '20px'}}>{post.text}</Text>
-          {(post.author.signature && post.author.signature.length) && <div><Divider dashed/><p>{post.author.signature}</p><Divider dashed/></div>}
-          <Space size={"large"} style={{display: 'flex', width: '100%'}}>
-            {post.createdAt && <Text style={{ fontSize: '11px'}} type='secondary' ><ClockCircleOutlined /> {format(new Date(post.createdAt), 'dd. MMM yyyy')}</Text>}
-            {
-              Math.round(created.getTime() / 1000) < Math.round(updated.getTime() / 1000) &&
-              <Text type="secondary" style={{ fontSize: '11px', fontStyle: 'italic' }}>Edited</Text>
-            }
-            {state.auth.token &&
-              (state.auth.user.roles.includes('ROLE_ADMIN') || state.auth.user.roles.includes('ROLE_MODERATOR')) &&
-              <span className="delete" onClick={() => postDelete(post)}>DELETE</span>
-            }
-            {state.auth.token &&
-                (state.auth.user.roles.includes('ROLE_ADMIN') || state.auth.user.roles.includes('ROLE_MODERATOR') || parseInt(state.auth.user.id, 10) === post.author.id) &&
-                <Link className="edit" to={"/post/edit/" + post.id}> EDIT</Link>
-            }
-          </Space>
-        </div>
+        <span>Posts: {post.author.threadAmount + post.author.postAmount}</span>
+        <span>Join date: {format(new Date(post.author.createdAt), 'dd. MMM yyyy')}</span>
+        {post.author.roles.map((role, i) => (
+          ((role.id === 1 && post.author.roles.length === 1) || (role.id !== 1 && post.author.roles.length > 1)) &&
+          <div key={i} style={{backgroundColor: role.bodyCss, textAlign: 'center', marginBottom: '5px'}}>
+            <Text style={{color: role.textCss, marginLeft: '5px'}}>{role.displayName}</Text>
+          </div>
+        ))}
+      </div>
+      <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+        <Text style={{marginBottom: '20px'}}>{post.text}</Text>
+        {(post.author.signature && post.author.signature.length) &&
+        <div><Divider dashed/><p>{post.author.signature}</p><Divider dashed/></div>}
+        <Space size={"large"} style={{display: 'flex', width: '100%'}}>
+          {post.createdAt && <Text style={{fontSize: '11px'}}
+                                   type='secondary'><ClockCircleOutlined/> {format(new Date(post.createdAt), 'dd. MMM yyyy')}
+          </Text>}
+          {
+            Math.round(created.getTime() / 1000) < Math.round(updated.getTime() / 1000) &&
+            <Text type="secondary" style={{fontSize: '11px', fontStyle: 'italic'}}>Edited</Text>
+          }
+          {state.auth.token &&
+          (state.auth.user.roles.includes('ROLE_ADMIN') || state.auth.user.roles.includes('ROLE_MODERATOR')) &&
+          <span className="delete" onClick={() => postDelete(post)}>DELETE</span>
+          }
+          {state.auth.token &&
+          (state.auth.user.roles.includes('ROLE_ADMIN') || state.auth.user.roles.includes('ROLE_MODERATOR') || parseInt(state.auth.user.id, 10) === post.author.id) &&
+          <Link className="edit" to={"/post/edit/" + post.id}> EDIT</Link>
+          }
+        </Space>
+      </div>
     </List.Item>
   )
 };
